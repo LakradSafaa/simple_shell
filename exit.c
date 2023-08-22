91d457f92738aae5c8ed641ad88118450eed9ba8
Safaa Lakrad <mlle.safaalakrad@gmail.com>
2023-08-22 15:32:59 -0700

    taskdone

--- exit.c
+++ exit.c
@@ -3,33 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-void exitShell() {
-    printf("Exiting the shell...\n");
-    exit(0);
+void exitShell()
+{
+	printf("Exiting the shell...\n");
+	exit(0);
 }
 
-int main() {
-    char input[256]; // Définir une taille maximale pour l'entrée du shell
+int main()
+{
+	char input[256];
+debut:
+	printf("Shell> ");
+	fgets(input, sizeof(input), stdin);
+	size_t len = strlen(input);
 
-   // while (1) {
-            debut:
-        printf("Shell> ");
-        fgets(input, sizeof(input), stdin);
-
-        // Supprimer le saut de ligne (\n) de l'entrée
-        size_t len = strlen(input);
-        if (len > 0 && input[len - 1] == '\n') {
-            input[len - 1] = '\0';
-        }
-
-        if (strcmp(input, "exit") == 0) {
-            exitShell(); // Appel de la fonction exitShell si l'utilisateur entre "exit"
-        } else {
-            // Traitement des autres commandes du shell ici...
-            printf("Commande non reconnue : %s\n", input);
-        }
-   // }
-   goto debut;
-
-    return 0;
+	if (len > 0 && input[len - 1] == '\n')
+		{
+		input[len - 1] = '\0';
+		}
+	if (strcmp(input, "exit") == 0)
+	{
+		exitShell(); "exit"
+	}
+	else
+	{
+		printf("Commande non reconnue : %s\n", input);
+	}
+	goto debut;
+	return (0);
 }
